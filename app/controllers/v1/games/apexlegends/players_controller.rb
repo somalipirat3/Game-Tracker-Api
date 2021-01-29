0575a1c6a96a3e75bee6344ac9415a767a581cba
mohamedsugroo <mohamedsugroo@gmail.com>
2021-01-29 00:16:16 +0000

    Player

--- app/controllers/v1/games/apexlegends/players_controller.rb
+++ app/controllers/v1/games/apexlegends/players_controller.rb
@@ -5,6 +5,26 @@ class V1::Games::Apexlegends::PlayersController < ApplicationController
         render json: @players
     end
 
+    def show
+        @player  = Player.find_by_id(params[:id])
+
+        @set_legends = []
+         
+        @legends = Legend.all.map 
+        
+        @legends.each do |legend|
+            @set_legends << {name: legend.name , id: legend.id, stats: Stat.where(legend_id: legend.id, player_id: @player.id)}
+        end
+
+        render json: {
+            player:  {
+                username: @player.username,
+                platform: @player.platform
+            },
+            legends: @set_legends
+        }
+    end
+
     def search
         username = params[:username]
         platform = params[:platform]
